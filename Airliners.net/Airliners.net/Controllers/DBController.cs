8745091c09eb53e46bdde47d3718bd70d2008dc7
gonzalmapi <gonzalmapi@hotmail.com>
2016-02-02 00:24:06 +0100

    Añadiendo Controlador de la Base de datos

---
+++ Airliners.net/Airliners.net/Controllers/DBController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using Airliners.net.Models;
+
+namespace Airliners.net.Controllers
+{
+    public class DBController : Controller
+    {
+       private AirlinersDBDataContext adb { get; set; }
+        public DBController()
+        {
+            adb = new AirlinersDBDataContext(ConfigurationManager.ConnectionStrings["Airliners_netConnectionString"].ConnectionString);
+        }
+        public Usuario getUsuario(string Nombre)
+        {
+            try
+            {
+                return adb.Usuarios.Single(u => u.Nombre == Nombre);
+            }
+            catch (InvalidOperationException)
+            {
+                return null;
+            }
+        }
+        
+    }
+}
