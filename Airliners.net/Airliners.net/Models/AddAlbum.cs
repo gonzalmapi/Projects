254cd91ba0ff674c6512c7bb2edeb3516cf2e513
gonzalmapi <gonzalmapi@hotmail.com>
2016-02-19 14:30:32 +0100

    Faltaba algo

---
+++ Airliners.net/Airliners.net/Models/AddAlbum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Airliners.net.Models
+{
+    public class AddAlbum
+    {
+        [Required(ErrorMessage = "Photo Name required")]
+        [Display(Name = "Photo Name")]
+        public string photo { get; set; }
+
+        [Required(ErrorMessage = "Name required")]
+        [Display(Name = "Name and Surname")]
+        public string name { get; set; }
+    }
+}
